03f77ddba1d28f1382c4837257943ceed15a6785
Miguel Oliveira <ctw02163@AdministorsMBP2.lan>
2022-06-29 22:46:58 +0100

    Update project

--- controllers/articles.js
+++ controllers/articles.js
@@ -14,7 +14,7 @@ class Articles {
     return results.rows;
   }
 
-  //post new article
+  //new article
   async newArticle(req) {
     let results = await db.query('INSERT INTO articles (article_name, number_pages, author_name, created_on, idiom) VALUES ($1, $2, $3, $4, $5) RETURNING *', [req.article_name, req.number_pages, req.author_name, req.created_on, req.idiom]).catch(console.log);
     return results.rows;
@@ -27,7 +27,7 @@ class Articles {
     return results.rows;
   }
 
-  //put article
+  //update article
   async updateArticle(reqParam, reqBody) {
     const id = reqParam;
     let results = await db.query('UPDATE articles SET article_name = $2, number_pages = $3, author_name = $4, created_on = $5, idiom = $6 WHERE id = $1', [id, reqBody.article_name, reqBody.number_pages, reqBody.author_name, reqBody.created_on, reqBody.idiom]).catch(console.log);
